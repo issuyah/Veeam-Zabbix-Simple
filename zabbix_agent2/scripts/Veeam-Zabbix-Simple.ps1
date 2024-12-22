897c3af6e6b8655cbf8f59e141affbde8036cf82
Renard <nolann@librenard.fr>
2024-12-22 15:56:39 +0100

    Added support for Tape Jobs to the script

--- zabbix_agent2/scripts/Veeam-Zabbix-Simple.ps1
+++ zabbix_agent2/scripts/Veeam-Zabbix-Simple.ps1
@@ -11,41 +11,49 @@ Connect-VBRServer -Server "localhost"
 
 # Get Veeam Jobs into a variable
 $backupJobs = Get-VBRJob
+$tapeJobs = Get-VBRTapeJob # Added to include Tape Jobs
+$allJobs = $backupJobs + $tapeJobs # Combined Backup and Tape Jobs
 
 # Prepare data for Zabbix Discovery
 $discoveryData = @()
-foreach ($job in $backupJobs) {
+foreach ($job in $allJobs) {
     # Clean Zabbix Job Name
     $cleanJobName = $job.Name -replace '[^a-zA-Z0-9_]', ''
     $discoveryData += @{"{#NOM}"=$cleanJobName}
 }
-#Convert data into Json and replace " with ?
+# Convert data into Json and replace " with ?
 $discoveryJson = $discoveryData | ConvertTo-Json -Compress
 $finaljson = $discoveryJson | ForEach-Object {$_ -replace '\"', '?'}
-#Send data to Zabbix server
+# Send data to Zabbix server
 & $zabbixSenderPath -z $zabbixServer -s $zabbixHost -k job.json -o $finaljson
 
 # Prepare job results for Zabbix
-foreach ($job in $backupJobs.where{$_.info.IsScheduleEnabled -eq 'True'}) {
+foreach ($job in $allJobs.where{$_.info.IsScheduleEnabled -eq 'True'}) {
     # Clean Zabbix Job Name
     $cleanJobName = $job.Name -replace '[^a-zA-Z0-9_]', ''
     $jobName = "job[" + $cleanJobName + "]"
-    $lastResult = $job.GetLastResult()
-
-    # #Send data to Zabbix server
-    & $zabbixSenderPath -z $zabbixServer -s $zabbixHost -k $jobName -o $lastResult
+    
+    # Get job result for Backup Jobs or status for Tape Jobs
+    if ($job.PSObject.TypeNames -contains "Veeam.Backup.Core.CJobTape") {
+        $jobStatus = "Enabled" # Tape Jobs don't have "last result"
+        & $zabbixSenderPath -z $zabbixServer -s $zabbixHost -k $jobName -o $jobStatus
+    } else {
+        $lastResult = $job.GetLastResult()
+        # Send data to Zabbix server
+        & $zabbixSenderPath -z $zabbixServer -s $zabbixHost -k $jobName -o $lastResult
+    }
     echo $jobName
 }
 
-foreach ($job in $backupJobs.where{$_.info.IsScheduleEnabled -ne 'True'}) {
-# Clean Zabbix Job Name
+foreach ($job in $allJobs.where{$_.info.IsScheduleEnabled -ne 'True'}) {
+    # Clean Zabbix Job Name
     $cleanJobName = $job.Name -replace '[^a-zA-Z0-9_]', ''
     $jobName = "job[" + $cleanJobName + "]"
 
-    #Send data to Zabbix server
+    # Send status as "Disabled" for all types of jobs
     & $zabbixSenderPath -z $zabbixServer -s $zabbixHost -k $jobName -o "Disabled"
     echo $jobName
 }
 
-# Disconnect from zabbix host
+# Disconnect from Veeam Server
 Disconnect-VBRServer
